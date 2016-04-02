3ccf4bc06960aa71c5499fd56e0aaef6a450a2c3
Daniel Moreto <dfmoreto@gmail.com>
2016-04-02 10:14:04 -0300

    Factory girl and Faker added and RSpec configured

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module IbmInterview
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        request_specs: false,
+        routing_specs: false,
+        helper_specs: false
+      g.fixture_replacement :factory_girl
+    end
+
   end
 end
